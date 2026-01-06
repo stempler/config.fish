8b209f3b53cb73d3c59d50f1303bb2337b4c8ee4
Simon Templer <simon@wetransform.to>
2026-01-06 08:53:15 +0100

    feat: add initial configuration

---
+++ conf.d/config.fish
@@ -0,0 +1,41 @@
+# configure SSH agent socket
+# XXX should this be shared?
+set -Ux SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+
+# Add local user binaries and scripts to PATH
+set -x PATH ~/bin $PATH
+set -x PATH ~/scripts $PATH
+
+# Set default editor
+set -x EDITOR nvim
+
+# Abbreviations
+
+# vi = nvim
+abbr -a vi nvim
+
+# Git
+abbr -a g git
+abbr -a gco git checkout
+abbr -a gci git commit
+abbr -a gcp git cherry-pick
+abbr -a gst git status
+abbr -a add git add
+abbr -a push git push
+abbr -a pull git pull
+abbr -a rebase git pull --rebase
+abbr -a fix git commit --fixup
+abbr -a gas git rebase -i --autosquash
+
+# Docker
+abbr -a d docker
+abbr -a dc docker compose
+
+# Build tool wrappers
+abbr -a gw ./gradlew
+abbr -a mw ./mvnw
+
+# Mise
+abbr -a m mise
+abbr -a mr mise run
+
