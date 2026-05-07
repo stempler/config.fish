1ad16856fcb0a3e99d769c12b2c77b1aa4ffa468
Simon Templer <simon@wetransform.to>
2026-05-07 08:28:08 +0200

    feat: highligh mise env name in prompt

--- functions/mise_env_prompt.fish
+++ functions/mise_env_prompt.fish
@@ -4,6 +4,8 @@ function mise_env_prompt --description 'Prompt component showing the active mise
     test (count $envs) -eq 0; and return
 
     set_color -d cyan
-    printf 'mise:%s' (string join ',' $envs)
+    printf 'mise:'
+    set_color --reset cyan
+    printf '%s' (string join ',' $envs)
     set_color normal
 end
